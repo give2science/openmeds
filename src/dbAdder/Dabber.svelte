48f2b8d96151fcd986f5125b0a180be46b1af426
nisten <nisten@give2science.com>
2022-11-28 18:18:43 -0500

    almost perfection

--- src/dbAdder/Dabber.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import FriendAdder from "./FriendAdder.svelte";
-    import FriendList from "./FriendList.svelte";
-  </script>
-  <h1>Paged Dexie liveQuery using Svelte</h1>
-  <FriendAdder />
-  <br/>
-  <FriendList />
--- src/dbAdder/drugCounter.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { liveQuery } from "dexie";
-    import { db } from "./db";
-  
-    let numFriends = liveQuery(() => db.friends.count());
-  </script>
-  
-  {$numFriends == null ? "loading..." : $numFriends}
